1d8870ba4c7a42e90fe1b7b2af7af0ad1c0bba81
jaosn <hanzhenyugg@gmail.com>
2015-10-16 18:26:08 -0400

    Most API thing done

--- nano_app.rb
+++ nano_app.rb
@@ -13,6 +13,7 @@ require 'pry-byebug'
 set :public_folder, File.dirname(__FILE__) + '/static'
 enable :sessions
 
+# find user info by username
 get '/api/v1/users/username/:name' do
   user = User.find_by name: params[:name]
   if user
@@ -22,7 +23,8 @@ get '/api/v1/users/username/:name' do
   end
 end
 
-get '/api/v1/users/id/:id' do
+#find user info by user_id
+get '/api/v1/users/userid/:id' do
 	user = User.find_by id: params[:id]
   if user
     user.to_json
@@ -31,7 +33,8 @@ get '/api/v1/users/id/:id' do
   end
 end
 
-get '/api/v1/tweets/id/:id' do
+# find tweet id by tweet_id
+get '/api/v1/tweets/tweetid/:id' do
 	user = Tweet.find_by id: params[:id]
   if user
     user.to_json
@@ -40,6 +43,7 @@ get '/api/v1/tweets/id/:id' do
   end
 end
 
+# find the user time_line by username
 get '/api/v1/tweets/username/:name' do
 	user = User.find_by name: params[:name]
 	user_id = user.id
@@ -52,6 +56,42 @@ get '/api/v1/tweets/username/:name' do
     error 404, {:error => "user not found"}.to_json
   end
 end
+# find the user time_line by user_id
+get '/api/v1/tweets/userid/:id' do
+	user = User.find_by id: params[:id]
+	user_id = user.id
+	comb_user = user_id && User.find(user_id)
+	tweets = comb_user ? Tweet.user_timeline(comb_user) : Tweet.all
+	# tweets=tweets.order(create_time: :desc)
+  if user
+    tweets.to_json
+  else
+    error 404, {:error => "user not found"}.to_json
+  end
+end
+
+# find the followers of a user by username
+get '/api/v1/followers/username/:name' do
+	user = User.find_by name: params[:name]
+  followers = user.followers
+	if user
+    followers.to_json
+  else
+    error 404, {:error => "user not found"}.to_json
+  end
+end
+
+# find the followers of a user by userid
+get '/api/v1/followers/userid/:id' do
+	user = User.find_by id: params[:id]
+  followers = user.followers
+	if user
+    followers.to_json
+  else
+    error 404, {:error => "user not found"}.to_json
+  end
+end
+
 
 
 
