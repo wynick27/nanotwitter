c2d37445f7842a8c4ff9c89b3cfc41ac9bc6e1e0
zhongqil <zhongqilpostoffice@gmail.com>
2015-10-26 13:50:25 -0400

    new changes

--- models/tweet.rb
+++ models/tweet.rb
@@ -8,15 +8,4 @@ class Tweet < ActiveRecord::Base
   def self.user_timeline(user) 
     Tweet.includes(:user).from("(#{user.tweets.to_sql} UNION #{Tweet.where(:user_id=>user.followed_users).to_sql}) as tweets")
   end
-  
-  def self.user_tweets(user)
-    Tweets.includes(:user).from("(#{user.tweets.to_sql} UNION #{Tweet.where(:tweet_id=>user.retweets).to_sql}) as tweets")
-  end
-  
-  def self.user_tweets_with_replies(user)
-    Tweets.includes(:user).from("(#{user.tweets.to_sql} UNION #{Tweet.where(:tweet_id=>user.retweets).to_sql}) as tweets")
-  end
-  def to_html
-    erb :show_tweet,:locals=>{:tweet=>self}
-  end
 end
