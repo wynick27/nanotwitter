9cce11c10a8aa17ed242264b606dc99878ae8984
wynick27 <wynick27@gmail.com>
2015-12-01 01:34:15 -0500

    Modified test interface

--- routes/test.rb
+++ routes/test.rb
@@ -23,9 +23,34 @@ require './utils/seed_generator'
       followers=FakeData::gen_followers(testuser,num)
       followers.to_json
   end
+  
+  
+  NanoTwitter.get '/test/status' do
+    testuser=User.find_by name: 'testuser'
+    {total_tweets:Tweet.all.count,
+    total_user:User.all.count,
+    total_followings: Follow.all.count,
+    testuser:testuser,
+    testuser_follower:testuser ? testuser.followers.count : 0,
+    testuser_following:testuser ? testuser.followed_users.count : 0,
+    testuser_tweets:testuser ? testuser.tweets.count : 0}.to_json
+  end
 
+  NanoTwitter.get '/test/reset/all' do
+    
+    Tweet.delete_all
+    User.delete_all
+    HashTag.delete_all
+    Favourite.delete_all
+    Follow.delete_all
+    Message.delete_all
+    Retweet.delete_all
+    Conversation.delete_all
+    ChatGroup.delete_all
+    "Done"
+  end
   #test interface
-  NanoTwitter.get '/test/reset' do
+  NanoTwitter.get '/test/reset/testuser' do
     
     testuser=User.find_by name: 'testuser'
     if !testuser.nil?
