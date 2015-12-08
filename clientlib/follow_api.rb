4aacc6e6d2cadec26f248ba79ca52534ea8c0726
wynick27 <wynick27@gmail.com>
2015-12-07 23:58:57 -0500

    merge

--- clientlib/follow_api.rb
+++ clientlib/follow_api.rb
@@ -6,39 +6,25 @@
 
       def self.find_follower_by_name(name)
         response = Typhoeus::Request.get("#{base_uri}/api/v1/followers/username/#{name}")
-        if response.code == 200
-            JSON.parse(response.body)
-        elsif response.code == 404
-            nil
-        else
-            raise response.body
-        end
+        checking(response)
       end
 
       def self.find_follower_by_id(id)
         response = Typhoeus::Request.get("#{base_uri}/api/v1/followers/userid/#{id}")
-        if response.code == 200
-            JSON.parse(response.body)
-        elsif response.code == 404
-            nil
-        else
-            raise response.body
-        end
+        checking(response)
       end
 
       def self.find_following_by_name(name)
         response = Typhoeus::Request.get("#{base_uri}/api/v1/followings/username/#{name}")
-        if response.code == 200
-            JSON.parse(response.body)
-        elsif response.code == 404
-            nil
-        else
-            raise response.body
-        end
+        checking(response)
       end
 
       def self.find_following_by_id(id)
         response = Typhoeus::Request.get("#{base_uri}/api/v1/followings/userid/#{id}")
+        checking(response)
+      end
+
+      def checking(response)
         if response.code == 200
             JSON.parse(response.body)
         elsif response.code == 404
@@ -47,4 +33,4 @@
             raise response.body
         end
       end
-end 
+end
--- clientlib/user_api.rb
+++ clientlib/user_api.rb
@@ -6,23 +6,20 @@ class User
 
 	def self.find_by_name(name)
 		response = Typhoeus::Request.get("#{base_uri}/api/v1/users/username/#{name}")
-		if response.code == 200
-			JSON.parse(response.body)
-		elsif response.code == 404
-			nil
-		else
-			raise response.body
-		end
+		checking(response)
 	end
 
 	def self.find_by_id(id)
 		response = Typhoeus::Request.get("#{base_uri}/api/v1/users/userid/#{id}")
+		checking(response)
+	end
+	def checking(response)
 		if response.code == 200
-			JSON.parse(response.body)
+				JSON.parse(response.body)
 		elsif response.code == 404
-			nil
+				nil
 		else
-			raise response.body
+				raise response.body
 		end
 	end
 end
