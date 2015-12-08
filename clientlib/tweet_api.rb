ffa83b538e2b22c02328679297b99fb6e792005c
jaosn <hanzhenyugg@gmail.com>
2015-12-07 21:12:36 -0500

    checking method

--- clientlib/tweet_api.rb
+++ clientlib/tweet_api.rb
@@ -6,28 +6,19 @@
 
     def self.find_tweet_by_id(id)
       response = Typhoeus::Request.get("#{base_uri}/api/v1/tweets/tweetid/#{id}")
-      if response.code == 200
-        JSON.parse(response.body)
-      elsif response.code == 404
-          nil
-      else
-          raise response.body
-      end
+      checking(response)
     end
 
       def self.find_user_tweet_by_username(name)
         response = Typhoeus::Request.get("#{base_uri}/api/v1/tweets/username/#{name}")
-        if response.code == 200
-            JSON.parse(response.body)
-        elsif response.code == 404
-            nil
-        else
-          raise response.body
-        end
+        checking(response)
       end
 
       def self.find_user_tweet_by_userid(id)
         response = Typhoeus::Request.get("#{base_uri}/api/v1/tweets/userid/#{id}")
+        checking(response)
+      end
+      def checking(response)
         if response.code == 200
             JSON.parse(response.body)
         elsif response.code == 404
